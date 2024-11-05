70d816cdd87017871166109578aedea2957b1daa
sashkoG <127833215+sashkoG@users.noreply.github.com>
2024-11-05 12:15:17 +0200

    Add files via upload

---
+++ SnimkaBackground/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnimkaBackground
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    this.BackgroundImage = Image.FromFile(openFileDialog1.FileName);
+                    this.BackgroundImageLayout = ImageLayout.Stretch;
+                }
+                catch
+                {
+                    MessageBox.Show("Проблем с файла!");
+                }
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
